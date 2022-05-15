4d41f904ca857a068b2a00c4708fca2983b7ae68
deepakvaswani1927 <62814208+deepakvaswani1927@users.noreply.github.com>
2022-05-15 17:33:03 +0530

    Update and rename Program7to14.js to Program7to16.js

--- Program7to16.js
+++ Program7to16.js
@@ -182,6 +182,41 @@ function HighCount() {
     }
 }
 
+function BubbleSort() {
+    const inputArr = [55, 20, 1, 34, 66, 55, 81, 60, 99, 11, 10, 30, 50, 44, 23, 14, 17, 29, 68, 100]
+
+    let len = 20;
+    for (let i = 0; i < len; i++) {     //resposible for all passes
+        for (let j = 0; j < len; j++) {     //executes for each pass means puts largest number at last
+            if (inputArr[j] > inputArr[j + 1]) {
+                let tmp = inputArr[j];
+                inputArr[j] = inputArr[j + 1];
+                inputArr[j + 1] = tmp;
+            }
+        }
+    }
+    console.log(inputArr);
+    document.write(inputArr);
+
+}
+
+function CreateTable()
+{
+rn = window.prompt("Input number of rows", 1);
+cn = window.prompt("Input number of columns",1);
+  
+ for(var r=0;r<parseInt(rn,10);r++)
+  {
+   var x=document.getElementById('myTable').insertRow(r);
+   for(var c=0;c<parseInt(cn,10);c++)  
+    {
+     var y=  x.insertCell(c);
+     y.innerHTML="("+r+","+c+")"; 
+    }
+   }
+}
+
+
 
 
 
